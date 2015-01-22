af20c8dd178b15f45c03b493aebdcb14ad64eca8
Richard Littauer <richard.littauer@gmail.com>
2015-01-21 17:27:11 -0800

    Added a simpler data file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 
-var removeContent = new RegExp('.*Loop="([A-Za-z ]*)".*\n',"g");
+// var removeContent = new RegExp('.*Loop="([A-Za-z ]*)".*\n',"g");
 
 var arr = [];
 
 gulp.task('default', function(){
-  gulp.src(['data.txt'])
+  gulp.src(['simple_data.txt'])
     // Remove header and footer lines
-    .pipe(replace(/<\?xml.*/g, ''))
-    .pipe(replace(/<.*resultset.*/g, ''))
-    .pipe(replace(removeContent, '$1\n'))
-    .pipe(replace(/(.*)\n(\1)/g, function(str, a, b){
-      if (a === b) {
-        return a;
-      }
-    }))
+    // .pipe(replace(/<\?xml.*/g, ''))
+    // .pipe(replace(/<.*resultset.*/g, ''))
+    // .pipe(replace(removeContent, '$1\n'))
+    .pipe(replace(/^(.*)(\r?\n)\1+$/g, ''))
+    // .pipe(replace(/(.*)\n(\1)/g, function(str, a, b){
+    //   if (a === b) {
+    //     return a;
+    //   }
+    // }))
     .pipe(gulp.dest('build/'));
 });
